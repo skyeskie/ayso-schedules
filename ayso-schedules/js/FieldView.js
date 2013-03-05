4569a46a91708e971d220a0cfea8a9156f433c39
Scott Yeskie <scott.k.yeskie@vanderbilt.edu>
2013-03-05 17:26:29 -0600

    Field maps First run. SVG files (not 49) need scaling and cleanup from AI output.

---
+++ ayso-schedules/js/FieldView.js
@@ -0,0 +1,62 @@
+var FieldView = {
+	type: "fields",
+	
+	//Could add this for a more descriptive of home region
+	//regionLong: ["Stryker Complex, "],
+	
+	svgFiles: ["img/Fields49.svg", "img/Fields105.svg", "img/Fields208.svg",
+	           "img/error.svg", "img/error.svg", "img/error.svg"],
+	
+	showIndex: function() {
+		$("#fields .listing").empty();
+		var myregion = DataControl.getRegion();
+		for(var i=0; i< app.regions.length; ++i) {
+			if(app.regions[i] == myregion) {
+				$("#fields .listing").prepend("<div>" +
+						"<h2>Region "+app.regions[i]+"</h2>" +
+						"<p>"+app.regionsLong[i]+"</p>" +
+						"<a data-role='button' href='#map?"+app.regions[i]+"'>Directions</a>" +
+						"<a data-role='button' href='#fields?"+app.regions[i]+"'>Field Map</a>" +
+					"</div>");
+			} else {
+				$("#fields .listing").append("<div class='width50 ui-bar-c'>" +
+					"<h2>Region "+app.regions[i]+"</h2>"+
+					"<p>"+app.regionsLong[i]+"</p>" +
+					"<a data-role='button' href='#map?"+app.regions[i]+"'>Map</a>" +
+					"<a data-role='button' href='#fields?"+app.regions[i]+"'>Fields</a>" +
+				"</div>");
+			}
+		}
+		
+		//Change page
+		var $page = $( "#fields" );
+		$page.page();
+		$( "#fields .listing" ).trigger( "create" );
+		$.mobile.changePage( $page );
+		location.hash = "#fields";
+		app.currentView = "#fields";
+	},
+
+	showDetail: function(offset) {
+		var r = null;
+		for(var i=0; i<app.regions.length; ++i) {
+			if(offset == app.regions[i]) r = i;
+		}
+		
+		if(r==null) {
+			r = 5; //Error
+		}
+		
+		console.log("<embed src='" +
+				this.svgFiles[r] +
+				"' type='image/svg+xml />");
+		
+		$("#field-map embed").attr("src", this.svgFiles[r]);
+
+		var $page = $( "#field-map" );
+		$page.page();
+		$.mobile.changePage( $page );
+		location.hash = "#fields?"+offset;
+		app.currentView = "#fields?"+offset;
+	}
+};
