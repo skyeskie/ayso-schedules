7a01db633e0058eb397ce12cc667bf60c6585a92
Scott Yeskie <scott@yeskie.com>
2015-10-19 16:48:44 -0700

    Configure Grunt processing

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function(grunt) {
+    require('load-grunt-config')(grunt);
+};
+
+//Potential modules to get later
+//grunt.loadNpmTasks('grunt-contrib-watch');      //Live editing recompile
+//grunt.loadNpmTasks('grunt-wiredep');            //Direct dependencies to bower
+//grunt.loadNpmTasks('grunt-contrib-imagemin');   //Minify images
+//grunt.loadNpmTasks('grunt-jsdoc');              //Create documentation (see also: ink-docstrap)
