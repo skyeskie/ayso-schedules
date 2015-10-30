963e34396bb927bb1304a2ce3cdbd1da2cf21151
Scott Yeskie <scott@yeskie.com>
2015-10-30 11:25:09 -0700

    Fix building to single-page HTML.
    Correctly wires in dependencies now from Bower

--- src/js/routing.js
+++ src/js/routing.js
@@ -1,8 +1,8 @@
 /* globals aysoApp */
-"use strict";
-
 aysoApp.config(['$routeProvider',
     function($routeProvider) {
+        "use strict";
+
         $routeProvider.when('/game/:gameId', {
             template: "game",
             controller: "GameInfoCtrl"
