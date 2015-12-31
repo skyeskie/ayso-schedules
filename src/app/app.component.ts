86e426dae25f0d7e938dbc6779b5ef49e8e5e414
Scott Yeskie <scott@yeskie.com>
2015-12-30 20:47:41 -0800

    Angular2 framework and dependency changes - Remove bower

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '../../node_modules/angular2/core.d';
+
+@Component({
+    selector: 'ayso-app',
+    template: '<p>TEST</p>'
+})
+
+export class AppComponent { }
---
+++ src/app/boot.ts
@@ -0,0 +1,4 @@
+import {bootstrap}    from 'angular2/platform/browser.d'
+import {AppComponent} from './app.component.ts'
+
+bootstrap(AppComponent);
