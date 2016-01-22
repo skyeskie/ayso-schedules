7a7561bca4bef85fc4c108a4a31ceac5b79c371f
Scott Yeskie <scott@yeskie.com>
2016-01-21 23:01:04 -0800

    Add editor configuration, fix Home template routing

--- src/views/home.ts
+++ src/views/home.ts
@@ -1,18 +1,20 @@
 import {Component, OnInit} from 'angular2/core';
 import {Router} from 'angular2/router';
 import {RouteParams} from "angular2/router";
+import {RouterLink} from 'angular2/router';
 
 @Component({
     selector: 'ayso-app',
+    directives: [RouterLink],
     template: `
 <div id="home" data-role="page">
     <img src="img/MainLogo.png" alt="AYSO Kansas" />
     <div class="main-buttons">
-        <a [routerLink]="['/schedules']" data-role="button">Schedules</a>
-        <a [routerLink]="['/regions']" data-role="button">Region Info</a>
-        <a [routerLink]="['/favorites']" data-role="button">My teams</a>
-        <a [routerLink]="['/twitter']" data-role="button">Cancellations</a>
-        <a [routerLink]="['/settings']" data-role="button">Settings</a>
+        <a [routerLink]="['/SchedulesMenu']" data-role="button">Schedules</a>
+        <a [routerLink]="['/RegionList']" data-role="button">Region Info</a>
+        <a [routerLink]="['/FavoritesSchedule']" data-role="button">My teams</a>
+        <a [routerLink]="['/TwitterView']" data-role="button">Cancellations</a>
+        <a [routerLink]="['/Settings']" data-role="button">Settings</a>
     </div>
 </div>
     `
