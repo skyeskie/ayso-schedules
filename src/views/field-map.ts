f358865da5cc1f3dd86d53c5f5d77f33cb7d9e37
Scott Yeskie <scott@yeskie.com>
2016-02-07 12:03:51 -0800

    Implement FieldView
    Resolves #39

--- src/views/field-map.ts
+++ src/views/field-map.ts
@@ -3,21 +3,27 @@ import {Router} from 'angular2/router';
 import {RouteParams} from "angular2/router";
 import {REGIONS, getRegionByNumber, Region} from '../cfg/regions';
 import {TitleBarComponent} from '../comp/title-bar.component';
+import {NgIf} from 'angular2/common';
 
 @Component({
-    directives: [TitleBarComponent],
+    directives: [TitleBarComponent, NgIf],
     template: `
     <title-bar></title-bar>
-    <div id="field">To be implemented</div>
+    <article class="container">
+        <div *ngIf="hasMap()">
+            <img [src]="region.mapFile" alt="Field Map {{region?.name}}" />
+        </div>
+        <div *ngIf="!hasMap()">
+            <h3>Field Map</h3>
+            <div role="alert" class="alert alert-danger m-a-2 text-xs-center">
+                <strong>Error</strong> Could not find map for region {{id}}
+            </div>
+        </div>
+    </article>
     `
 })
-
-//<div id="field-map" data-role="page" class="page">
-//  <canvas id="canvas"></canvas>
-//  <div id="svg-dump"></div>
-//</div>
-
 export default class FieldMapView implements OnInit {
+    public id;
     private region: Region;
 
     constructor(
@@ -26,9 +32,11 @@ export default class FieldMapView implements OnInit {
     ) {}
 
     ngOnInit() {
-        let id = this._routeParams.get("region");
-        this.region = getRegionByNumber(parseInt(id, 10));
-        //$field.empty();
-       // $field.load(this.region.mapFile);
+        this.id = this._routeParams.get("region");
+        this.region = getRegionByNumber(parseInt(this.id, 10));
+    }
+
+    hasMap() {
+        return this.region && this.region.mapFile;
     }
 }
