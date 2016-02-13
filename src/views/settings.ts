608df9f2b65666b09b0975bee3a80808f9732143
Scott Yeskie <scott@yeskie.com>
2016-02-12 21:06:29 -0800

    Display previous update on settings page

--- src/views/settings.ts
+++ src/views/settings.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, Injectable} from 'angular2/core';
-import {Control, NgFor, FORM_DIRECTIVES} from 'angular2/common';
+import {Control, DatePipe, NgFor, FORM_DIRECTIVES} from 'angular2/common';
 
 import {REGIONS, Region} from '../cfg/regions';
 import {TitleBarComponent} from '../comp/title-bar.component';
@@ -7,6 +7,7 @@ import {DataControlService} from '../dao/data-control.service';
 
 @Component({
     directives:[NgFor, TitleBarComponent, FORM_DIRECTIVES],
+    pipes:[DatePipe],
     template: `
     <title-bar></title-bar>
     <article class="container">
@@ -26,7 +27,7 @@ import {DataControlService} from '../dao/data-control.service';
             <h4 class="card-title">Update data</h4>
             <p>The app normally checks for updates on start.
             Use this if you think you missed an update.</p>
-            <div><strong>Last updated:</strong> {{lastUpdate}}</div>
+            <div><strong>Last updated:</strong> {{lastUpdate | date:'MMMdjhm'}}</div>
             <button class="btn btn-info-outline">Update now</button>
             <p id="update-result"></p>
         </div>
@@ -50,7 +51,7 @@ export default class SettingsView {
     public defaultRegion = new Control('');
 
     public regions:Region[];
-    public lastUpdate:String;
+    public lastUpdate:Date;
 
     constructor(
         private dataConrol: DataControlService
@@ -59,8 +60,9 @@ export default class SettingsView {
 
         this.dataConrol.settings.getRegionNumber().then(r => this.defaultRegion.updateValue(r.toString()));
 
+        this.lastUpdate = this.dataConrol.getLastUpdate();
+
         this.defaultRegion.valueChanges.subscribe(val => {
-            console.log('Saving region:' + val);
             let regionNum:Number = parseInt(val,10);
             this.dataConrol.settings.setRegion(regionNum);
         });
@@ -71,6 +73,8 @@ export default class SettingsView {
     }
 
     forceRefresh(): void {
-        this.dataConrol.update(true);
+        this.dataConrol.update(true).then(updated => {
+            this.lastUpdate = updated;
+        });
     }
 }
