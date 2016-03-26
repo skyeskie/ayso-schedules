a1b0e32a0a2046a4f50e2994c1560d4a7c21f435
Scott Yeskie <scott@yeskie.com>
2016-03-25 17:44:16 -0700

    Add SVG pan-zoom typing

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -3,3 +3,4 @@
 /// <reference path="jasmine/jasmine.d.ts" />
 /// <reference path="twitter/twitter.d.ts" />
 /// <reference path="jasmine-expect/jasmine-expect.d.ts" />
+/// <reference path="svg-pan-zoom/svg-pan-zoom.d.ts" />
